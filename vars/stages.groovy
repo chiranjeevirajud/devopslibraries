3a210fae09951b125ec079668d03e837a38d02e1
chiranjeevirajud <101318214+chiranjeevirajud@users.noreply.github.com>
2022-04-10 18:03:23 +0530

    Update stages.groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -4,11 +4,11 @@ def call(String stageName){
   {
   sh "mvn clean package"
   }
-  else if ("${stageName"} == "SQ Report")
+  else if ("${stageName}" == "SQ Report")
   {
     sh "mvn clean sonar:sonar"
   }
-  else if ("${stageName"} == "ArtifactToNexus")
+  else if ("${stageName}" == "ArtifactToNexus")
   {
     sh "mvn clean deploy"
   }
